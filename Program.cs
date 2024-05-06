30054dd52392cd1c2093cdd84cfde485f5c34358
Matheus Xavier Soares <mxsoares12@hotmail.com>
2024-05-06 17:46:44 -0300

    Solução do desafio de criação de sistema e abstração de celular com POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone sNokia = new Nokia(numero: "912345678", modelo: "Lumia 920", imei: "111111111", memoria: 256);
+sNokia.Ligar();
+sNokia.InstalarAplicativo("Duolingo");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone:");
+Smartphone sIPhone = new Iphone(numero: "998765432", modelo:"13 Pro Max", imei: "222222222", memoria: 64);
+sIPhone.ReceberLigacao();
+sIPhone.InstalarAplicativo("X");
